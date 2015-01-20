32118b3aa512ca3e749078a2da433ff4429ed536
Zaur Abasmirzoev <zaur.kavkaz@gmail.com>
2015-01-21 00:53:11 +0300

    clear test

--- gol_test.go
+++ gol_test.go
@@ -10,12 +10,3 @@ func TestLevelString(t *testing.T) {
 	assert := assert.New(t)
 	assert.Equal("FATAL", FATAL.String())
 }
-
-//func TestViewText(t *testing.T) {
-//assert := assert.New(t)
-//r := &Record{Time: time.Now(), Pid: 123, Level: ERROR, Body: "hello"}
-//r.Context = []string{"line1", "line2"}
-//buf, _ := ViewText(r)
-//assert.Contains(buf.String(), "hello")
-//t.Log(buf.String())
-//}
